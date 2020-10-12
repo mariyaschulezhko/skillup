9a79525ffd3d7e460aa669f2c66847b938f77735
mariyaschulezhko <shulezhkomariia@gmail.com>
2020-10-13 00:30:45 +0300

    homework arrays

---
+++ samples/arrays.php
@@ -0,0 +1,27 @@
+<?php
+$a1 = ['test', 'qwerty', 123];
+$a2 = [4 => 'test', 2 => 'qwerty' , 233 => 123];
+$a3 = [
+    'key1' => 'Test',
+    'color' => 'Red',
+    'car' => 'Subaru',
+    'CAT'
+];
+$a4 = [
+    [
+        'id' => 1,
+        'name' => 'PHP BASICS',
+        'students' => [
+            [
+                'name' => 'Med Max',
+                'age' => 33
+            ],
+            [
+                'name' => 'Malibu',
+                'age' => 52
+            ]
+        ]
+    ]
+];
+
+var_dump($a4);
